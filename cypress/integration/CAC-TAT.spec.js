889b1fcc5bd07732abd9e8d31ae5220186917ae7
Vinicius Da Silveira Moraes <vinicius.moraes@barte.com>
2024-11-21 19:34:30 -0300

    Returning the code to it's functional state

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -152,10 +152,10 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
 // TESTA LINKS QUE ABREM EM OUTRA PÁGINA DO NAVEGADOR/////////////////////////////////////////////////////////////
     it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', ()=>{
-        cy.ge('#privacy a').should('have.attr', 'target', '_blank')
+        cy.get('#privacy a').should('have.attr', 'target', '_blank')
     })
     it('acessa a página da política de privacidade removendo o target e então clicando no link', ()=>{
-        cy.ge('#privacy a')   
+        cy.get('#privacy a')   
             .invoke('removeAttr', 'target')
             .click()
         cy.contains('Talking About Testing').should('be.visible')
